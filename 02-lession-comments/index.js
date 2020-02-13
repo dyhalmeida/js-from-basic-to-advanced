5b9027c4b3554148a4ff1f736eb74fc34c469524
dyhalmeida <dyhalmeida@gmail.com>
2020-02-13 10:22:18 -0300

    lesson 02

---
+++ 02-lession-comments/index.js
@@ -0,0 +1,12 @@
+console.log(`--- Comentário de linha ---`);
+
+// O código abaixo apresenta uma saída no console com a frase "Hello World!"
+console.log(`Hello World!`);
+
+console.log(`--- Comentário de bloco ---`);
+/**
+ * O código abaixo apresenta
+ * uma saída no console com a frase
+ * "Hello World 2!"
+ */
+console.log(`Hello World 2!`);
