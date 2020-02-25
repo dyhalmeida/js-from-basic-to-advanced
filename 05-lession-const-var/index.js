ab90cf751735710a1c63acf031de5b08b2acbacf
dyhalmeida <dyhalmeida@gmail.com>
2020-02-25 13:36:40 -0300

    lesson 05

---
+++ 05-lession-const-var/index.js
@@ -0,0 +1,19 @@
+/**
+ * Constantes const:
+ * Possuem escopo.
+ * Não pode ser declarada sem atribuição de valor.
+ * Não pode ser redeclarada.
+ * Não pode ter uma retribuição de valor.
+ */
+
+const maleName = "John";
+const femaleName = "Maria";
+const sonName = "Eduardo";
+
+console.log(`
+  ${maleName} was born in 1984.
+  In 2000 ${maleName} met ${femaleName}.
+  ${maleName} married ${femaleName} in 2012.
+  ${femaleName} had 1 child with ${maleName} in 2015.
+  ${maleName}'s son is called ${sonName}.
+`);
