8581b391ebbb0e633327417ac55d96f8ed858d67
Diego Almeida <dyhalmeida@gmail.com>
2020-04-03 10:00:47 -0300

    lesson forEach array

---
+++ 60-lesson-forEach-array/index.js
@@ -0,0 +1,7 @@
+const numbers = [1, 4, 7, 9, 45, 2, 7, 12, 57, 34, 9, 44];
+
+// numbers.forEach(value => console.log(value));
+
+let total = 0;
+numbers.forEach(value => total += value);
+console.log(total);
