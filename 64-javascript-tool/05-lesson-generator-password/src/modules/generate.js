7c680720f2c10a87c5121e0e877e35c413e7eee3
Diego Almeida <dyhalmeida@gmail.com>
2020-04-08 11:02:33 -0300

    function generator password

---
+++ 64-javascript-tool/05-lesson-generator-password/src/modules/generate.js
@@ -0,0 +1,20 @@
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const generateCharUpperCase = () => String.fromCharCode(rand(65, 91));
+const generateCharLowerCase = () => String.fromCharCode(rand(97, 123));
+const generateCharNumber = () => String.fromCharCode(rand(48, 58));
+const generateCharSymbol = () => rand(0, `!@#$%&*()_-+=[]{}^~/?|<>`.length);
+
+const generatePassword = (quantity, hasUpperCase, hasLowerCase, hasNumber, hasSymbol) => {
+  const chars = [];
+  quantity = Number(quantity);
+  
+  for (let i = 0; i < quantity; i++) {
+    hasUpperCase && chars.push(generateCharUpperCase());
+    hasLowerCase && chars.push(generateCharLowerCase());
+    hasNumber && chars.push(generateCharNumber());
+    hasSymbol && chars.push(generateCharSymbol());
+  }
+  return chars.join('').slice(0, quantity);
+}
+
+export { generatePassword as default };
